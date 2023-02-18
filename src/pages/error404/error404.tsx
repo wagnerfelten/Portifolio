3cc525f15c7ccb9e4d0c7b8ba48ee541dd045d79
WagnerRodrigues <felten19@gmail.com>
2023-02-18 18:17:13 -0300

    Criado link para voltar para pege  home na page de error404

--- src/pages/error404/error404.tsx
+++ src/pages/error404/error404.tsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom";
 import imgChoro from "../../assets/img/bighead.svg";
-import "./style.css"
+import "./style.css";
 
 const errorPage = () => {
-    return(
-        <div className="content-container">
-            <h1>ERROR 404 </h1>
+  return (
+    <div className="content-container">
+      <h1>ERROR 404 </h1>
 
-            <img src={imgChoro} alt="Boneco cancelado" />
-        </div>
-    )
-}
+      <img src={imgChoro} alt="Boneco cancelado" />
+
+      <div>
+        <Link to="/"> To go back</Link>
+      </div>
+    </div>
+  );
+};
 
 export default errorPage;
